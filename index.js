2997c57823a83225c1f630cee866cb5cc528fae2
karlavago <karlavago19@gmail.com>
2026-04-20 09:37:44 -0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,8 @@
+function saludar() {
+    var nombre = "Cristi";
+    if (nombre == "Cristi") {
+        console.log("Hola");
+    }
+    // El robot detectará que esta variable no se usa:
+    var error = 10; 
+}
